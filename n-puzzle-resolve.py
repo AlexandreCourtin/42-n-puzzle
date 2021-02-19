805b44ed800b00ea6eff161c0e804676cc686419
acourtin <acourtin@student.42.fr>
2021-02-19 16:02:28 +0100

    solver read file in arg

--- n-puzzle-resolve.py
+++ n-puzzle-resolve.py
@@ -1,2 +1,11 @@
+import argparse
+
 if __name__ == "__main__":
-    print('execute solver')
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("file", type=open, help="The puzzle to solve.")
+
+	args = parser.parse_args()
+
+	if args.file:
+		print(args.file.read())
