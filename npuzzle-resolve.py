f40ce4897baa45a31c3910fa2ec89c6644fa6792
AlexandreCourtin <acourtin@student.42.fr>
2021-02-21 20:38:23 +0100

    new python typo

--- npuzzle-resolve.py
+++ npuzzle-resolve.py
@@ -19,33 +19,33 @@ if __name__ == '__main__':
 
 	print(Bcolors.YELLOW + '===== reading file =====' + Bcolors.ENDC)
 
-	lineCount = 0
-	rowCount = 0
-	columnCount = 0
+	line_count = 0
+	row = 0
+	column = 0
 	length = 0
-	hasLength = False
-	startMatrix = []
-	desiredMatrix = []
+	has_length = False
+	start_matrix = []
+	desired_matrix = []
 
 	while True:
-		print('line {}:'.format(lineCount))
+		print('line {}:'.format(line_count))
 		line = args.file.readline().split()
 
 		for char in line:
 			if char.isnumeric():
 				print(char, end = '|')
-				if not hasLength:
+				if not has_length:
 					length = int(char)
-					hasLength = True
-					startMatrix = [ [ 0 for i in range(length) ] for j in range(length) ]
-					desiredMatrix = [ [ 0 for i in range(length) ] for j in range(length) ]
+					has_length = True
+					start_matrix = [ [ 0 for i in range(length) ] for j in range(length) ]
+					desired_matrix = [ [ 0 for i in range(length) ] for j in range(length) ]
 				else:
-					if rowCount < length and columnCount < length:
-						startMatrix[rowCount][columnCount] = int(char)
-						columnCount += 1
-						if columnCount >= length:
-							columnCount = 0
-							rowCount += 1
+					if row < length and column < length:
+						start_matrix[row][column] = int(char)
+						column += 1
+						if column >= length:
+							column = 0
+							row += 1
 			else:
 				break
 		print('')
@@ -53,26 +53,26 @@ if __name__ == '__main__':
 		if not line:
 			break
 
-		lineCount += 1
+		line_count += 1
 
 	print(Bcolors.YELLOW + '\n===== file summary =====' + Bcolors.ENDC)
 	print('heuristic function is {}'.format(Bcolors.GREEN + args.function + Bcolors.ENDC))
 	print('length is {}'.format(Bcolors.GREEN + str(length) + Bcolors.ENDC))
 	print('matrix is:')
-	for array in startMatrix:
+	for array in start_matrix:
 		print(Bcolors.GREEN + str(array) + Bcolors.ENDC)
 
 	print(Bcolors.YELLOW + '\n===== check if only one zero in matrix =====' + Bcolors.ENDC)
-	zeroNumber = 0
-	for array in startMatrix:
+	zero_number = 0
+	for array in start_matrix:
 		for number in array:
 			if number == 0:
-				zeroNumber += 1
+				zero_number += 1
 
-	if zeroNumber > 1:
+	if zero_number > 1:
 		print(Bcolors.RED + 'too much zeros' + Bcolors.ENDC)
 		sys.exit(1)
-	elif zeroNumber < 1:
+	elif zero_number < 1:
 		print(Bcolors.RED + 'no zeros' + Bcolors.ENDC)
 		sys.exit(1)
 	else:
@@ -85,10 +85,10 @@ if __name__ == '__main__':
 	y = 0
 	i = 1
 	while i < length * length:
-		desiredMatrix[y][x] = i
+		desired_matrix[y][x] = i
 		i += 1
 
-		if y + directionY >= length or x + directionX >= length or desiredMatrix[y + directionY][x + directionX] != 0:
+		if y + directionY >= length or x + directionX >= length or desired_matrix[y + directionY][x + directionX] != 0:
 			if directionX == 1:
 				directionX = 0
 				directionY = 1
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 		y += directionY
 
 	print('desired matrix is:')
-	for array in desiredMatrix:
+	for array in desired_matrix:
 		print(Bcolors.GREEN + str(array) + Bcolors.ENDC)
 
 	print(Bcolors.YELLOW + '\n===== resolving n-puzzle with '
@@ -119,78 +119,77 @@ if __name__ == '__main__':
 				result += str(char)
 		return result
 
-	def heuristic(nextMatrix):
-		heuristicValue = 0
+	def heuristic(next_matrix):
+		heuristic_value = 0
 		i = 0
 		while i < length:
 			j = 0
 			while j < length:
-				puzzleNumber = nextMatrix[i][j]
-
-				if puzzleNumber != 0:
+				next_matrix_value = next_matrix[i][j]
+				if next_matrix_value != 0:
 					ii = 0
 					while ii < length:
 						jj = 0
 						while jj < length:
-							desiredNumber = desiredMatrix[ii][jj]
-							if desiredNumber == puzzleNumber:
-								heuristicValue += abs(i - ii) + abs(j - jj)
+							desiredNumber = desired_matrix[ii][jj]
+							if desiredNumber == next_matrix_value:
+								heuristic_value += abs(i - ii) + abs(j - jj)
 							jj += 1
 						ii += 1
 
 				j += 1
 			i += 1
-		return heuristicValue
+		return heuristic_value
 
-	def changeTile(currentMatrix, paramX, paramY):
-		changedTileMatrix = [ [ 0 for i in range(length) ] for j in range(length) ]
+	def change_tile(current_matrix, paramX, paramY):
+		result_matrix = [ [ 0 for i in range(length) ] for j in range(length) ]
 		i = 0
 		while i < length:
-			changedTileMatrix[i] = currentMatrix[i].copy()
+			result_matrix[i] = current_matrix[i].copy()
 			i += 1
 
-		isDone = False
+		done = False
 		i = 0
-		while i < length and not isDone:
+		while i < length and not done:
 			j = 0
-			while j < length and not isDone:
-				if changedTileMatrix[i][j] == 0:
+			while j < length and not done:
+				if result_matrix[i][j] == 0:
 					if j + paramX < length and i + paramY < length and j + paramX >= 0 and i + paramY >= 0:
-						changedTileMatrix[i][j] = changedTileMatrix[i + paramY][j + paramX]
-						changedTileMatrix[i + paramY][j + paramX] = 0
+						result_matrix[i][j] = result_matrix[i + paramY][j + paramX]
+						result_matrix[i + paramY][j + paramX] = 0
 					else:
-						changedTileMatrix = None
-					isDone = True
+						result_matrix = None
+					done = True
 				j += 1
 			i += 1
-		return changedTileMatrix
+		return result_matrix
 
-	def neighbors(currentMatrix):
+	def neighbors(current_matrix):
 		neighbors_list = [[], [], [], []]
-		neighbors_list[0] = changeTile(currentMatrix, 1, 0)
-		neighbors_list[1] = changeTile(currentMatrix, -1, 0)
-		neighbors_list[2] = changeTile(currentMatrix, 0, 1)
-		neighbors_list[3] = changeTile(currentMatrix, 0, -1)
+		neighbors_list[0] = change_tile(current_matrix, 1, 0)
+		neighbors_list[1] = change_tile(current_matrix, -1, 0)
+		neighbors_list[2] = change_tile(current_matrix, 0, 1)
+		neighbors_list[3] = change_tile(current_matrix, 0, -1)
 		return neighbors_list
 	
-	def printMatrix(m):
+	def print_matrix(m):
 		for row in m:
 			print(row)
 
 	def print_path_from(finalMatrix):
 		tab = []
-		previousMatrix = came_from[matrix_to_id(finalMatrix)]
-		while previousMatrix:
-			tab.append(previousMatrix)
-			previousMatrix = came_from[matrix_to_id(previousMatrix)]
+		previous_matrix = came_from[matrix_to_id(finalMatrix)]
+		while previous_matrix:
+			tab.append(previous_matrix)
+			previous_matrix = came_from[matrix_to_id(previous_matrix)]
 		tab = tab[::-1]
 		for t in tab:
-			printMatrix(t)
+			print_matrix(t)
 			print(Bcolors.YELLOW + '================' + Bcolors.ENDC)
-		printMatrix(desiredMatrix)
+		print_matrix(desired_matrix)
 
 	matrix_queue = queue.Queue()
-	matrix_queue.put(startMatrix, 0)
+	matrix_queue.put(start_matrix, 0)
 
 	selected_opened_state_count = 0
 	maximum_state_count = 1
@@ -199,36 +198,36 @@ if __name__ == '__main__':
 	# id_to_matrix = dict()
 	came_from = dict()
 	cost_so_far = dict()
-	# id_to_matrix[matrix_to_id(startMatrix)] = startMatrix
-	came_from[matrix_to_id(startMatrix)] = None
-	cost_so_far[matrix_to_id(startMatrix)] = 0
+	# id_to_matrix[matrix_to_id(start_matrix)] = start_matrix
+	came_from[matrix_to_id(start_matrix)] = None
+	cost_so_far[matrix_to_id(start_matrix)] = 0
 
 	start_time = time.time()
 	while not matrix_queue.empty():
-		currentMatrix = matrix_queue.get()
+		current_matrix = matrix_queue.get()
 		selected_opened_state_count += 1
 		current_state_count -= 1
 
-		if currentMatrix == desiredMatrix:
+		if current_matrix == desired_matrix:
 			print(Bcolors.GREEN + 'win!' + Bcolors.ENDC)
-			print_path_from(currentMatrix)
+			print_path_from(current_matrix)
 			print('time = ' + Bcolors.GREEN + str(time.time() - start_time) + Bcolors.ENDC)
 			print('selected opened state count = ' + Bcolors.GREEN + str(selected_opened_state_count) + Bcolors.ENDC)
 			print('maximum state count in memory = ' + Bcolors.GREEN + str(maximum_state_count) + Bcolors.ENDC)
 			break
 
-		for nextMatrix in neighbors(currentMatrix):
-			if nextMatrix != None:
-				currentId = matrix_to_id(currentMatrix)
-				nextId = matrix_to_id(nextMatrix)
-				new_cost = cost_so_far[currentId] + 1
-				next_matrix_heuristic = heuristic(nextMatrix)
-				if nextId not in cost_so_far or new_cost < cost_so_far[nextId]:
-					cost_so_far[nextId] = new_cost
+		for next_matrix in neighbors(current_matrix):
+			if next_matrix != None:
+				current_id = matrix_to_id(current_matrix)
+				next_id = matrix_to_id(next_matrix)
+				new_cost = cost_so_far[current_id] + 1
+				next_matrix_heuristic = heuristic(next_matrix)
+				if next_id not in cost_so_far or new_cost < cost_so_far[next_id]:
+					cost_so_far[next_id] = new_cost
 					new_cost_with_heuristic = new_cost + next_matrix_heuristic
-					matrix_queue.put(nextMatrix, -new_cost_with_heuristic)
-					came_from[nextId] = currentMatrix
-					# id_to_matrix[nextId] = nextMatrix
+					matrix_queue.put(next_matrix, -new_cost_with_heuristic)
+					came_from[next_id] = current_matrix
+					# id_to_matrix[next_id] = next_matrix
 					current_state_count += 1
 					if maximum_state_count < current_state_count:
 						maximum_state_count = current_state_count
